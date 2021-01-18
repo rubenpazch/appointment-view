38c5c9ca522debc4669a91b1dbee3dd72d932b77
Ruben Paz Chuspe <rubenpazchuspe@outlook.com>
2021-01-18 18:50:59 -0500

    add registration form

--- src/app/modules/Auth/pages/Registration.js
+++ src/app/modules/Auth/pages/Registration.js
@@ -1,16 +1,90 @@
-import React from 'react';
+/* eslint-disable react/no-unused-prop-types */
+/* eslint-disable react/forbid-prop-types */
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+// import { Link } from 'react-router-dom';
+import {
+  Formik,
+  Form,
+  Field,
+  ErrorMessage,
+} from 'formik';
+// import PropTypes from 'prop-types';
+import * as Yup from 'yup';
+import { setToken } from '../_redux/authAction';
+// import * as auth from '../_redux/authRedux';
+import { login } from '../_redux/authService';
 
-const Registration = () => (
-  <div>
+const Registration = () => {
+  const [loading, setLoading] = useState(false);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    setLoading(false);
+    return () => setLoading(false);
+  }, []);
+
+  const LoginSchema = Yup.object().shape({
+    username: Yup.string()
+      .min(3, 'Minimum 3 symbols')
+      .max(50, 'Maximum 50 symbols')
+      .required('this field is required'),
+    password: Yup.string()
+      .min(3, 'Minimum 3 symbols')
+      .max(50, 'Maximum 50 symbols')
+      .required('this field is required'),
+  });
+
+  const enableLoading = () => {
+    setLoading(true);
+  };
+  const disableLoading = () => {
+    setLoading(false);
+  };
+
+  return (
     <div>
-      <h3 className="font-size-h1">
-        new text in registration
-      </h3>
-      <p>
-        Enter your details to create your account
-      </p>
+      <h1>Any place in your app!</h1>
+      <Formik
+        initialValues={{ username: '', password: '' }}
+        validationSchema={LoginSchema}
+        onSubmit={(values, { setSubmitting, setStatus }) => {
+          enableLoading();
+          setTimeout(() => {
+            login(values.username, values.password)
+              .then(({ data: { token, username } }) => {
+                dispatch(setToken({ token, username }));
+                disableLoading();
+              })
+              .catch(() => {
+                disableLoading();
+                setSubmitting(false);
+                setStatus('not working');
+              });
+          }, 1400);
+        }}
+      >
+        {({ isSubmitting, status }) => (
+          <Form>
+            <h1>{status}</h1>
+            <Field type="username" name="username" />
+            <ErrorMessage name="username" component="div" />
+            <Field type="password" name="password" />
+            <ErrorMessage name="password" component="div" />
+            <button type="submit" disabled={isSubmitting}>
+              <span>Sign In</span>
+              {loading && (
+              <div className="spinner-border text-primary" role="status">
+                <span className="sr-only">Loading...</span>
+              </div>
+              )}
+            </button>
+
+          </Form>
+        )}
+      </Formik>
     </div>
-  </div>
-);
+  );
+};
 
 export default Registration;
