2d89f56b3989700628a574164734a9e6c7e2be30
Ruben Paz Chuspe <rubenpazchuspe@outlook.com>
2021-01-18 17:53:14 -0500

    add eslint config file

--- src/app/modules/Auth/pages/Registration.js
+++ src/app/modules/Auth/pages/Registration.js
@@ -1,19 +1,16 @@
 import React from 'react';
 
-const Registration = () => {
-  return (
+const Registration = () => (
+  <div>
     <div>
-      <div>
-        <h3 className="font-size-h1">
-          new text in registration
-          {loading}
-        </h3>
-        <p>
-          Enter your details to create your account
-        </p>
-      </div>
+      <h3 className="font-size-h1">
+        new text in registration
+      </h3>
+      <p>
+        Enter your details to create your account
+      </p>
     </div>
-  );
-};
+  </div>
+);
 
 export default Registration;
