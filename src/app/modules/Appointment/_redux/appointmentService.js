8dd163cda75f2a29a7fdd8be83091d743e63e826
Ruben Paz Chuspe <rubenpazchuspe@outlook.com>
2021-01-25 21:13:40 -0500

    Add service to save appointment

--- src/app/modules/Appointment/_redux/appointmentService.js
+++ src/app/modules/Appointment/_redux/appointmentService.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const APPOINTMENT_BY_DATE_AREA = 'http://localhost:3000/api/v1/getappointments';
+export const CREATE_APPOINTMENT = 'http://localhost:3000/api/v1/appointments';
 
 // eslint-disable-next-line camelcase
 export function getListAppointmentByDateService(department_id, date) {
@@ -8,6 +9,20 @@ export function getListAppointmentByDateService(department_id, date) {
     appointment: {
       department_id,
       date,
+    },
+  });
+}
+
+// eslint-disable-next-line camelcase
+export function saveAppointment(appointmentDate, startTime, endTime, user_id, doctor_id) {
+  return axios.post(CREATE_APPOINTMENT, {
+    appointment: {
+      appointmentDate,
+      startTime,
+      endTime,
+      status: true,
+      user_id,
+      doctor_id,
     },
   });
 }
