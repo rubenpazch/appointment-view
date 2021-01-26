331b2088dc55a7b49da37f59bfcd8a689bf4331b
Ruben Paz Chuspe <rubenpazchuspe@outlook.com>
2021-01-25 21:13:24 -0500

    Add reducer to save appointment

--- src/app/modules/Appointment/_redux/appointmentReducer.js
+++ src/app/modules/Appointment/_redux/appointmentReducer.js
@@ -3,6 +3,7 @@ const initialState = {
 };
 
 export const FETCH_LIST_APPOINTMENT_BY_DATE_SERVICE = 'FETCH_LIST_APPOINTMENT_BY_DATE_SERVICE';
+export const SAVE_APPOINTMENT = 'SAVE_APPOINTMENT';
 
 export const appointmentReducer = (state = initialState, action) => {
   switch (action.type) {
