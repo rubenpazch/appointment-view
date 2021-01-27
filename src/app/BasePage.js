3cf3e75f759d4a03cd59c394471e1ff60e28fe3b
Ruben Paz Chuspe <rubenpazchuspe@outlook.com>
2021-01-27 17:45:41 -0500

    Add dashboard component

--- src/app/BasePage.js
+++ src/app/BasePage.js
@@ -2,13 +2,15 @@ import React, { lazy, Suspense } from 'react';
 import { Redirect, Switch, Route } from 'react-router-dom';
 
 const Appointment = lazy(() => import('./modules/Appointment'));
+const Dashboard = lazy(() => import('./modules/Dashboard'));
 // const Register = lazy(() => import('./modules/Appointment/Register'));
 
 export default function BasePage() {
   return (
     <Suspense>
       <Switch>
-        <Redirect exact from="/" to="/Appointments" />
+        <Redirect exact from="/" to="/Dashboard" />
+        <Route path="/Dashboard" component={Dashboard} />
         <Route path="/Appointments" component={Appointment} />
       </Switch>
     </Suspense>
