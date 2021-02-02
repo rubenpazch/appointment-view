fe726f0bf265077fa65dc436cc0a56c3ce1cc7ed
ruben paz chuspe <rubenpazchuspe@outlook.com>
2021-02-02 08:24:41 -0500

    Add option to show appointment by doctor

--- src/app/modules/Dashboard/Pages/Appointment.js
+++ src/app/modules/Dashboard/Pages/Appointment.js
@@ -1,6 +1,9 @@
 /* eslint-disable no-unused-vars */
 import moment from 'moment';
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
+import {
+  useLocation,
+} from 'react-router-dom';
 import styled from 'styled-components';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
@@ -17,6 +20,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getListAppointmentByDateService, saveAppointment } from '../../Appointment/_redux/appointmentService';
 import { ToastContext } from '../../../../components/ToastContextProvider';
 import ModalAppointment from './ModalAppointment';
+import { setListByDateService } from '../../Appointment/_redux/appointmentAction';
+import AppointmentDetail from '../../Appointment/pages/AppointmentDetail';
 
 const Wrapper = styled.div`
 `;
@@ -37,6 +42,11 @@ const LabelAppointmentDepartment = styled.div`
   margin: 0;
 `;
 
+const ContentAvailability = styled.div`
+  padding: 15px;
+  margin: 0;
+`;
+
 const defaultDepartment = {
   attributes: {
     name: 'Not selected',
@@ -54,6 +64,22 @@ const defaultDepartment = {
   },
   type: '',
 };
+
+const defaultDoctorCalendar = [{
+  attributes: {
+    endDate: '',
+    endTime: '',
+    id: 0,
+    shiftinterval: 0,
+    startDate: '',
+    startTime: '',
+    totalHours: 0,
+    user_id: 0,
+  },
+  id: 0,
+  type: '',
+}];
+
 const getTextFromObject = text => {
   const expresionRegular = /\[|\]/;
   const result = text.split(expresionRegular);
@@ -64,12 +90,16 @@ const getTextFromObject = text => {
 };
 
 const Appointment = () => {
+  const dispatch = useDispatch();
   const { notify } = useContext(ToastContext);
   // eslint-disable-next-line camelcase
   const { departments, user_id } = useSelector(state => state.tokenStore);
   const [currentDepartment, setCurrentDepartment] = useState(defaultDepartment);
   const [appointmentDateState, setAppointmentDateState] = useState(moment(new Date()).format('YYYY-MM-DD'));
   const [open, setOpen] = React.useState(false);
+  const { doctorcalendars, doctors, doctorsusers } = useSelector(state => state.tokenStore);
+  const [doctorCalendarByDepartment, setDoctorCalendarByDepartment] = useState(null);
+  const [availability, setAvailability] = useState();
   const [startTimeState, setStartTimeState] = useState('08:00');
   const [intervalTimeState, setIntervalTimeState] = useState('');
   const [listShiftDetailsState, setListShiftDetailsState] = useState([
@@ -82,6 +112,9 @@ const Appointment = () => {
       status: true,
     },
   ]);
+  const doctorCalendarId = new URLSearchParams(useLocation().search).get('id');
+  const [filterAppointmentsBy, setFilterAppointmentsBy] = useState();
+  console.log({ doctorCalendarId });
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -119,6 +152,117 @@ const Appointment = () => {
       });
   };
 
+  useEffect(() => {
+    const DoctorCalendarObject = doctorcalendars
+      .find(item => item.id === doctorCalendarId);
+
+    const dataDepartment = departments.find(
+      item => item.id === DoctorCalendarObject.relationships.department.data.id,
+    );
+
+    const doctorCalendar = doctorcalendars.filter(
+      item => item.attributes.startDate === appointmentDateState
+      && item.id === doctorCalendarId,
+    );
+
+    getListAppointmentByDateService(
+      DoctorCalendarObject.relationships.department.data.id,
+      appointmentDateState,
+    )
+      .then(({ data }) => {
+        console.log({ data });
+        dispatch(setListByDateService(data));
+        setFilterAppointmentsBy(data.data);
+        const appointmentList = data.data;
+        const includedList = data.included;
+        const arrayOfPerson = [];
+        const arrayOfUsers = [];
+        for (let indexIL = 0; indexIL < includedList.length; indexIL += 1) {
+          if (includedList[indexIL].type === 'person') {
+            arrayOfPerson.push(includedList[indexIL]);
+          }
+          if (includedList[indexIL].type === 'user') {
+            arrayOfUsers.push(includedList[indexIL]);
+          }
+        }
+
+        const arrayHours = [];
+        doctorCalendar.filter(x => {
+          const obj = {
+            startTime: moment.utc(x.attributes.startTime),
+            endTime: moment.utc(x.attributes.endTime),
+            user_id: x.attributes.user_id,
+            totalHours: x.attributes.totalHours,
+            shiftinterval: x.attributes.shiftinterval,
+            totalShift: ((
+              Number(x.attributes.totalHours) * 60)
+                / (Number(x.attributes.shiftinterval))),
+            userInformation: null,
+            doctorpersonInformation: null,
+          };
+          arrayHours.push(obj);
+          return defaultDoctorCalendar;
+        });
+
+        for (let indexAH = 0; indexAH < arrayHours.length; indexAH += 1) {
+          arrayHours[indexAH].userInformation = doctorsusers
+            .find(x => Number(x.id)
+          === Number(arrayHours[indexAH].user_id));
+        }
+
+        for (let indexAH = 0; indexAH < arrayHours.length; indexAH += 1) {
+          arrayHours[indexAH].doctorpersonInformation = doctors
+            .find(x => Number(x.id)
+          === Number(arrayHours[indexAH].userInformation.attributes.person_id));
+        }
+
+        const ShiftByHour = [];
+        let indexKey = 1;
+        for (let indexj = 0; indexj < arrayHours.length; indexj += 1) {
+          const r = 0;
+          let nextStartTime = moment.utc(arrayHours[indexj].startTime);
+          for (let r = 0; r < arrayHours[indexj].totalShift; r += 1) {
+            const obj = {
+              id: indexKey,
+              startTime: moment.utc(nextStartTime).format('HH:mm'),
+              endTime: moment.utc(nextStartTime).add(15, 'minutes').format('HH:mm'),
+              status: false,
+              doctor_id: arrayHours[indexj].user_id,
+              patient_id: '',
+              firstName: ' ... ',
+              lastName: '',
+            };
+            ShiftByHour.push(obj);
+            nextStartTime = moment.utc(nextStartTime).add(15, 'minutes');
+            indexKey += 1;
+          }
+        }
+
+        for (let indexh = 0; indexh < appointmentList.length; indexh += 1) {
+          const foundIndex = ShiftByHour.findIndex(
+            x => x.startTime === moment.utc(appointmentList[indexh].attributes.startTime).format('HH:mm'),
+          );
+          if (foundIndex !== -1) {
+            ShiftByHour[foundIndex].status = true;
+            ShiftByHour[foundIndex].patient_id = appointmentList[indexh].attributes.user_id;
+            const objectUser = arrayOfUsers.find(u => Number(u.id)
+            === Number(appointmentList[indexh].attributes.user_id));
+            const objectPerson = arrayOfPerson.find(p => Number(p.id)
+            === Number(objectUser.attributes.person_id));
+            ShiftByHour[foundIndex].firstName = objectPerson.attributes.firstName;
+            ShiftByHour[foundIndex].lastName = objectPerson.attributes.lastName;
+          }
+        }
+
+        setAvailability(ShiftByHour);
+      })
+      .catch(error => {
+        // console.log({ error });
+        // setSubmitting(false);
+        // setStatus('not working');
+      });
+  }, [doctorCalendarId]);
+
   return (
     <Wrapper>
       <ContentLogout className="d-flex flex-row justify-content-between">
@@ -129,8 +273,24 @@ const Appointment = () => {
           open={open}
           handleClose={handleClose}
           handleSubmitModal={handleSubmitModal}
+          selectObjectList={listShiftDetailsState}
         />
       </ContentLogout>
+      <ContentAvailability>
+        { availability !== null && typeof (availability) !== 'undefined'
+          ? availability.map(item => (
+            <AppointmentDetail
+              key={item.id}
+              time={item.startTime}
+              endTime={item.endTime}
+              status={item.status}
+              patient={`${item.firstName} ${item.lastName} `}
+              service="ODONTOLOGIA BASICA"
+              office="Consultorio 105 (Ubicado en el piso 4 modulo 3)"
+            />
+          ))
+          : <h1>no data</h1> }
+      </ContentAvailability>
     </Wrapper>
   );
 };
