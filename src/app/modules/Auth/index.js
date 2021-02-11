d6e57d0aed2a1974d2a48ba793fe54478281e015
Ruben Paz Chuspe <rubenpazchuspe@outlook.com>
2021-02-10 22:07:43 -0500

    Chhange to arrow function

--- src/app/modules/Auth/index.js
+++ src/app/modules/Auth/index.js
@@ -1,3 +1,2 @@
-export { AuthPage } from './pages/AuthPage';
+export { default as AuthPage } from './pages/AuthPage';
 export { default as Logout } from './pages/Logout';
-// export * from './_redux/authRedux';
