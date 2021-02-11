fd699db1935db45075e4c4862a61cfeb6ced60e0
Ruben Paz Chuspe <rubenpazchuspe@outlook.com>
2021-02-10 22:07:57 -0500

    Delete not used imports

--- src/app/modules/Auth/pages/AuthPage.js
+++ src/app/modules/Auth/pages/AuthPage.js
@@ -5,10 +5,7 @@ import {
 import styled from 'styled-components';
 import toAbsoluteUrl from '../../../../helpers/assetsHelpers';
 import Login from './Login';
-// import ContentRoute from '../../../../layout/components/ContentRoute';
-// import Registration from './Registration';
 import StepperRegistration from './StepperRegistration';
-// import ForgotPassword from './ForgotPassword';
 
 const Wrapper = styled.div`  
   min-height: 100vh;
@@ -47,86 +44,85 @@ const WrapperLogin = styled.div`
   height: 100%;  
 `;
 
-// eslint-disable-next-line import/prefer-default-export
-export function AuthPage() {
-  return (
-    <>
-      <Wrapper className="d-flex flex-column">
-        <MainContent className="d-flex flex-column flex-sm-column flex-md-column flex-lg-row">
-          <LeftAside className="d-flex">
-            <ContentLeftAside
-              className="d-flex flex-column flex-sm-column flex-md-column flex-lg-column justify-content-between"
-            >
-              <Link to="/">
-                <img
-                  alt="Logo"
-                  src={toAbsoluteUrl('/media/logos/logo.png')}
-                />
-              </Link>
-              <div className="d-flex flex-column m-3">
-                <h3>
-                  Welcome to Clinic Appointment
-                </h3>
-                <p>
-                  Managment of clinical Appointment, Multiple Services, Patient Registration.
-                </p>
-              </div>
-              <div className="d-flex flex-row justify-content-between m-3">
-                <div>
-                  &copy; Appointment App
-                </div>
-                <div>
-                  <Link to="/terms">
-                    Contact
-                  </Link>
-                </div>
-              </div>
-            </ContentLeftAside>
-          </LeftAside>
-
-          <ContentAside className="d-flex flex-column overflow-hidden">
-            <WrapperSignUp className="d-flex flex-row justify-content-end pr-5 pt-5">
-              <span>
-                Dont have an account yet?
-              </span>
-              <Link
-                to="/auth/registration"
-                id="kt_login_signup"
-                className="pl-3"
-              >
-                Sign Up!
-              </Link>
-            </WrapperSignUp>
-            <WrapperLogin className="d-flex align-items-center">
-              <Switch>
-                <Route path="/auth/login">
-                  <Login />
-                </Route>
-                <Route path="/auth/registration">
-                  <StepperRegistration />
-                </Route>
-                <Redirect from="/auth" exact to="/auth/login" />
-                <Redirect to="/auth/login" />
-              </Switch>
-            </WrapperLogin>
-            <div className="d-flex d-lg-none flex-column flex-sm-row justify-content-between align-items-center mt-5 p-5">
-              <div className="text-dark-50 font-weight-bold order-2 order-sm-1 my-2">
+const AuthPage = () => (
+  <>
+    <Wrapper className="d-flex flex-column">
+      <MainContent className="d-flex flex-column flex-sm-column flex-md-column flex-lg-row">
+        <LeftAside className="d-flex">
+          <ContentLeftAside
+            className="d-flex flex-column flex-sm-column flex-md-column flex-lg-column justify-content-between"
+          >
+            <Link to="/">
+              <img
+                alt="Logo"
+                src={toAbsoluteUrl('/media/logos/logo.png')}
+              />
+            </Link>
+            <div className="d-flex flex-column m-3">
+              <h3>
+                Welcome to Clinic Appointment
+              </h3>
+              <p>
+                Managment of clinical Appointment, Multiple Services, Patient Registration.
+              </p>
+            </div>
+            <div className="d-flex flex-row justify-content-between m-3">
+              <div>
                 &copy; Appointment App
               </div>
-              <div className="d-flex order-1 order-sm-2 my-2">
-                <Link
-                  to="/terms"
-                  className="text-dark-75 text-hover-primary ml-4"
-                >
+              <div>
+                <Link to="/terms">
                   Contact
                 </Link>
               </div>
             </div>
-          </ContentAside>
+          </ContentLeftAside>
+        </LeftAside>
 
-        </MainContent>
+        <ContentAside className="d-flex flex-column overflow-hidden">
+          <WrapperSignUp className="d-flex flex-row justify-content-end pr-5 pt-5">
+            <span>
+              Dont have an account yet?
+            </span>
+            <Link
+              to="/auth/registration"
+              id="kt_login_signup"
+              className="pl-3"
+            >
+              Sign Up!
+            </Link>
+          </WrapperSignUp>
+          <WrapperLogin className="d-flex align-items-center">
+            <Switch>
+              <Route path="/auth/login">
+                <Login />
+              </Route>
+              <Route path="/auth/registration">
+                <StepperRegistration />
+              </Route>
+              <Redirect from="/auth" exact to="/auth/login" />
+              <Redirect to="/auth/login" />
+            </Switch>
+          </WrapperLogin>
+          <div className="d-flex d-lg-none flex-column flex-sm-row justify-content-between align-items-center mt-5 p-5">
+            <div className="text-dark-50 font-weight-bold order-2 order-sm-1 my-2">
+              &copy; Appointment App
+            </div>
+            <div className="d-flex order-1 order-sm-2 my-2">
+              <Link
+                to="/terms"
+                className="text-dark-75 text-hover-primary ml-4"
+              >
+                Contact
+              </Link>
+            </div>
+          </div>
+        </ContentAside>
 
-      </Wrapper>
-    </>
-  );
-}
+      </MainContent>
+
+    </Wrapper>
+  </>
+);
+
+export default AuthPage;
