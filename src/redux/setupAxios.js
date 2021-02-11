f032ac0a22b6a01bd0ff650812d08caa962e18fe
Ruben Paz Chuspe <rubenpazchuspe@outlook.com>
2021-02-10 22:08:24 -0500

    Update setup axios config file

--- src/redux/setupAxios.js
+++ src/redux/setupAxios.js
@@ -1,9 +1,30 @@
-/* eslint-disable no-param-reassign */
+/* eslint-disable no-unused-vars */
 export default function setupAxios(axios) {
-  axios.interceptors.response.use(response => response, error => {
-    if (error.response.status === 401) {
-      return Promise.reject(error.response);
+  axios.interceptors.request.use(request => {
+    console.log({ request });
+    return request;
+  },
+  error => {
+    console.log({ error });
+    return Promise.reject(error);
+  });
+  axios.interceptors.response.use(response => {
+    console.log('it is response', { response });
+    return response;
+  },
+  error => {
+    // console.log('response', { error });
+    const { status, data, config } = error.response;
+    if (status === 500) {
+      // console.log('error 500');
+      // return error;
+      throw new Error(`${error.config.url} not found`);
     }
-    return error;
+    if (status === 401) {
+      // console.log('not authorized');
+      throw new Error(`${error.config.url} not found`);
+    }
+    // return Promise.reject(error);
+    throw error;
   });
 }
